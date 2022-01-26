3e484b8c4f9fae116d9b570df012d7df608ab8fc
Pablo Jj <pablojjimenez0@gmail.com>
2022-01-26 23:09:25 +0100

    Revert "Tfg1"

--- server/app.py
+++ server/app.py
@@ -1,66 +1,10 @@
-from dataclasses import Field
-
-from pydantic import BaseModel
+from server.routes.vegetable import router as VegetableRouter
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
+app.include_router(VegetableRouter, tags=["vegetables"], prefix="/vegetables")
 
 
-# app.include_router(VegetableRouter, tags=["vegetables"], prefix="/vegetables")
-
-class ItemTest(BaseModel):
-    ping: str = ''
-    otro: int = 0
-
-
-@app.get(
-    "/ping",
-    tags=["Test Connection"],
-    description='Endpoint for test connection'
-)
+@app.get("/", tags=["Root"])
 async def read_root():
-    return {'ping': 'pong'}
-
-
-@app.get("/casuas", tags=["defunciones-por-causas-capitulos-sexo-y-grupos-de-edad-ecm-identificador"])
-async def obtener_causas():
-    import pandas as pd
-    import json
-
-    # json string
-
-    # read json to data frame
-    df = pd.read_json('data/49075.json')
-    nombres = df['Nombre']
-    _s = set()
-    for row in nombres.values:
-        _s.add(row.split('.')[1])
-    return {
-        'data': _s
-    }
-
-
-@app.get("/edades", tags=["defunciones-por-causas-capitulos-sexo-y-grupos-de-edad-ecm-identificador"])
-async def obtener_edades():
-    """
-    Is it documentation?
-    ----
-    Cosas de implementación
-
-    :return: un json feo
-    """
-    import pandas as pd
-    import json
-
-    # json string
-
-    # read json to data frame
-    df = pd.read_json('data/49075.json')
-    nombres = df['Nombre']
-    _s = set()
-    for row in nombres.values:
-        _s.add(row.split('.')[3].strip())
-    return {
-        'data': sorted(_s)
-    }
+    return {"message": "Welcome to this fantastic app!"}
---
+++ server/models/vegetable.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class VegetableSchema(BaseModel):
+    name: str = Field(...)
+    color: str = Field(...)  # color: EmailStr
+    calories: str = Field(...)
+    vitamins: List[str] = Field(...)
+    season: List[str] = Field(...)
+    origin: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Tomato",
+                "color": "RED",
+                "calories": 33,
+                "vitamins": [
+                    "B2"
+                ],
+                "season": [
+                    "WINTER",
+                    "SPRING",
+                    "SUMMER",
+                    "AUTUMN"
+                ],
+                "origin": "Spain"
+            }
+        }
+
+
+class UpdateVegetableModel(BaseModel):
+    name: str = Field(...)
+    calories: str = Field(...)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Tomato",
+                "calories": 33,
+            }
+        }
+
+
+def VegetableModel(data, message):
+    return {
+        "data": data,
+        "size": len(data),
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
